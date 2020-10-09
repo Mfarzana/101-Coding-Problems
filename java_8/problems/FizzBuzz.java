ebea654a8ee3d5a2fe3adfcee75b8b4633fbd03d
Mfarzana <mfarzana1133@gmail.com>
2020-10-10 01:31:08 +0900

    Date time API, Stream collection Parralel sort , consumer examle

--- java_8/problems/FizzBuzz.java
+++ java_8/problems/FizzBuzz.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package java_8.problems;
+package _101_Coding_Problems.java_8.problems;
 import java.util.*;
 /**
  *
